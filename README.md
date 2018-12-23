f2da2440152691c5230e8e017700345621b48f78
BobZAnnapolis <bahbzee@gmail.com>
2018-12-23 12:09:53 -0500

    added article creation, simple user autherntication

--- README.md
+++ README.md
@@ -1,9 +1,28 @@
 # go_webWithGin
-golang web &amp; microservices project using gin framework, from a web url :
+golang web &amp; microservices project using gin framework, from :
 
 https://semaphoreci.com/community/tutorials/building-go-web-applications-and-microservices-using-gin
 
-see notes for addl details
+see notes file for addl details
+
+Pre-REQUISITES:
+- properly set up Go environment
 
 Only uses 1 dependency :
 - go get -u github.com/gin-gonic/gin
+
+- after you git clone and install above dependency, from command line
+  - go build -o main and then access from localhost:8080
+  - or, "go run ..." and list ALL the go files
+
+- invocation:
+ - /a/view/xx where 'xx' is the number of an article displayed
+[GIN-debug] GET    /                         --> main.showIndexPage (4 handlers)
+[GIN-debug] GET    /u/login                  --> main.showLoginPage (5 handlers)
+[GIN-debug] POST   /u/login                  --> main.performLogin (5 handlers)
+[GIN-debug] GET    /u/logout                 --> main.logout (5 handlers)
+[GIN-debug] GET    /u/register               --> main.showRegistrationPage (5 handlers)
+[GIN-debug] POST   /u/register               --> main.register (5 handlers)
+[GIN-debug] GET    /a/view/:article_id       --> main.getArticle (4 handlers)
+[GIN-debug] GET    /a/create                 --> main.showArticleCreationPage (5 handlers)
+[GIN-debug] POST   /a/create                 --> main.createArticle (5 handlers)
--- common_test.go
+++ common_test.go
@@ -1,5 +1,3 @@
-// common_test.go
-
 package main
 
 import (
@@ -11,22 +9,24 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+var tmpUserList []user
 var tmpArticleList []article
 
-// this function is used for setup before executing the test functions
+// This function is used to do setup before executing the test functions
 func TestMain(m *testing.M) {
-	// set Gin to Test mode
+	//Set Gin to Test Mode
 	gin.SetMode(gin.TestMode)
 
-	// run the other tests
+	// Run the other tests
 	os.Exit(m.Run())
 }
 
-// helper function to create a router during testing
+// Helper function to create a router during testing
 func getRouter(withTemplates bool) *gin.Engine {
 	r := gin.Default()
 	if withTemplates {
 		r.LoadHTMLGlob("templates/*")
+		r.Use(setUserStatus())
 	}
 	return r
 }
@@ -45,13 +45,27 @@ func testHTTPResponse(t *testing.T, r *gin.Engine, req *http.Request, f func(w *
 	}
 }
 
+// This is a helper function that allows us to reuse some code in the above
+// test methods
+func testMiddlewareRequest(t *testing.T, r *gin.Engine, expectedHTTPCode int) {
+	// Create a request to send to the above route
+	req, _ := http.NewRequest("GET", "/", nil)
+
+	// Process the request and test the response
+	testHTTPResponse(t, r, req, func(w *httptest.ResponseRecorder) bool {
+		return w.Code == expectedHTTPCode
+	})
+}
+
 // This function is used to store the main lists into the temporary one
 // for testing
 func saveLists() {
+	tmpUserList = userList
 	tmpArticleList = articleList
 }
 
 // This function is used to restore the main lists from the temporary one
 func restoreLists() {
+	userList = tmpUserList
 	articleList = tmpArticleList
 }
